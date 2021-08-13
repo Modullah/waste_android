d8240c35baa75353b9896ed5b9b4320e8152350b
mo <modullah@gmail.com>
2021-08-12 22:42:18 -0500

    Image picker is working and url is being downloaded from firebase. Major breakthrough, finally.

--- lib/screens/list.dart
+++ lib/screens/list.dart
@@ -1,51 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-//import 'package:image_picker/image_picker.dart';
-//import 'package:firebase_storage/firebase_storage.dart';
-//import 'package:waste/screens/gallery.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
 
 class List extends StatefulWidget {
-  const List({
-    Key? key,
-  }) : super(key: key);
-  //final Function (String imageUrl) onFileChanged;  required this.onFileChanged
+  const List({Key? key}) : super(key: key);
   @override
   _ListState createState() => _ListState();
 }
 
 class _ListState extends State<List> {
-  var loading = true;
-  File? image; //File
+  File? image;
   var url;
 
-  // Future pickImage(ImageSource source) async {
-  //   final image = await ImagePicker().pickImage(source: source);
-  //   final imagePath = File(image!.path);
-  //   Reference reference =
-  //       FirebaseStorage.instance.ref().child(image.toString());
-  //   UploadTask uploadTask = reference.putFile(imagePath);
+  Future pickImage(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+    final imagePath = File(image!.path);
+    Reference reference =
+        FirebaseStorage.instance.ref().child(image.toString());
+    UploadTask uploadTask = reference.putFile(imagePath);
 
-  //   uploadTask.whenComplete(() async {
-  //     url = await reference.getDownloadURL();
-  //     print(url);
-  //   }).catchError((onError) {
-  //     print(onError);
-  //   });
+    uploadTask.whenComplete(() async {
+      url = await reference.getDownloadURL();
+      print(url);
+    }).catchError((onError) {
+      print(onError);
+    });
 
-  //   print(url);
-  // }
+    print(url);
+  }
 
   CollectionReference ref = FirebaseFirestore.instance.collection('waste');
 
   @override
   Widget build(BuildContext context) {
-    return loading == false ? crclProgInd() : list();
-  }
-
-  Widget list() {
     return Scaffold(
-      appBar: AppBar(title: Text('Waste')),
+      appBar: AppBar(
+        title: Text('Waste'),
+        centerTitle: true,
+      ),
       floatingActionButton: camera(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: StreamBuilder(
@@ -56,9 +50,9 @@ class _ListState extends State<List> {
                 : ListView.builder(
                     itemCount: snapshot.data.docs.length,
                     itemBuilder: (context, index) {
-                      DocumentSnapshot post = snapshot.data.docs[index];
+                      DocumentSnapshot waste = snapshot.data.docs[index];
                       return ListTile(
-                        leading: Text(post['quantity'].toString()),
+                        leading: Text(waste['quantity'].toString()),
                         title: Text('Placeholder'),
                       );
                     });
@@ -66,20 +60,21 @@ class _ListState extends State<List> {
     );
   }
 
-  crclProgInd() {
-    return Center(child: CircularProgressIndicator());
-  }
-
   Widget camera() {
     return FloatingActionButton(
       child: Icon(Icons.camera_alt),
-      onPressed: () => {},
+      onPressed: () => pickImage(ImageSource.gallery),
     );
   }
+}
 
-  pickImgGallery() {
+// crclProgInd() {
+//   return Center(child: CircularProgressIndicator());
+// }
+
+
+  //pickImgGallery() {
     //pickImage(ImageSource.gallery);
     //Navigator.push(context, MaterialPageRoute(builder: (context) => Gallery()));
-  }
+  //}
   //onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Camera()))
-}
